9c2b963621050c75fc1946d110e79fd3fcdd9604
test <wwng@2333.me>
2023-02-07 19:07:40 +0800

    Update main.php

--- main.php
+++ main.php
@@ -6,7 +6,9 @@ use Workerman\Protocols\Http\Response;
 require_once __DIR__ . '/vendor/autoload.php';
 
 $http_worker = new Worker("http://0.0.0.0:2333");
-$http_worker->count = 4;
+$http_worker->count = 1;
+$http_worker->name = 'generate_204';
+$http_worker->user = 'nobody';
 $http_worker->onMessage = function(TcpConnection $connection, Request $request)
 {
     $connection->send(new Response(204, [
