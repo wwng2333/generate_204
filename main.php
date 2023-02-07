807721b78dbe0ae1e26bae61dcc998ebdb751559
test <wwng@2333.me>
2023-02-07 19:01:22 +0800

    Create main.php

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php
+use Workerman\Worker;
+use Workerman\Connection\TcpConnection;
+use Workerman\Protocols\Http\Request;
+use Workerman\Protocols\Http\Response;
+require_once __DIR__ . '/vendor/autoload.php';
+
+$http_worker = new Worker("http://0.0.0.0:2333");
+$http_worker->count = 4;
+$http_worker->onMessage = function(TcpConnection $connection, Request $request)
+{
+    $connection->send(new Response(204, [
+        'X-Powered-by' => 'github.com/wwng2333/generate_204'
+    ], ''));
+};
+
+Worker::runAll();
