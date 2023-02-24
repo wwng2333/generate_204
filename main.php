9849aa310ddcbca47546e8cb5d4fe3ecdaea15a9
test <wwng@2333.me>
2023-02-24 18:18:20 +0800

    Update main.php

--- main.php
+++ main.php
@@ -3,17 +3,20 @@ use Workerman\Worker;
 use Workerman\Connection\TcpConnection;
 use Workerman\Protocols\Http\Request;
 use Workerman\Protocols\Http\Response;
+
 require_once __DIR__ . '/vendor/autoload.php';
+
+$response = new Response(204, [
+    'X-Powered-by' => 'github.com/wwng2333/generate_204',
+    'Connection' => 'close'
+]);
 
 $http_worker = new Worker("http://0.0.0.0:2333");
 $http_worker->count = 1;
 $http_worker->name = 'generate_204';
-$http_worker->onMessage = function(TcpConnection $connection, Request $request)
-{
-    $connection->close(new Response(204, [
-        'X-Powered-by' => 'github.com/wwng2333/generate_204',
-        'Connection' => 'close'
-    ], ''));
+$http_worker->onMessage = function (TcpConnection $connection, Request $request) {
+    global $response;
+    $connection->close($response);
 };
 
 Worker::runAll();
